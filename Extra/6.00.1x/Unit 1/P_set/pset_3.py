269f019f2f286ed018a9691338f1a244129b9a80
Husayn01 <hussainiahmed222@gmail.com>
2025-02-12 22:22:08 +0100

    Practiced MITx Course

--- Extra/6.00.1x/Unit 1/P_set/pset_3.py
+++ Extra/6.00.1x/Unit 1/P_set/pset_3.py
@@ -13,7 +13,44 @@
 # we suggest that you move on to a different part of the course.
 # If you have time, come back to this problem after you've had a break and cleared your head.
 
-if "s" >= "z":
-    print("Yes")
-else:
-    print("No")
+# if "s" >= "z":
+#     print("Yes")
+# else:
+#     print("No")
+
+
+# s = 'azcbobobegghakl'
+# Longest_substring = s[0]
+
+# for i in range(len(s) - 2):
+#     print(s[i])
+#     print(s[i  + 1])
+#     if s[i] <= s[i  + 1]:
+#         Longest_substring += s[i  + 1]
+#         # if Longest_substring[-1] < 
+#         print(Longest_substring)
+#         print("Yes")
+#     else:
+#         print("No")
+
+
+s = 'azcbobobegghakl'  # Example input
+longest = s[0]
+current = s[0]
+
+for i in range(1, len(s)):
+    if s[i] >= current[-1]:
+        print(current[-1])
+        current += s[i]
+        if len(current) > len(longest):
+            longest = current
+    else:
+        if len(current) > len(longest):
+            longest = current
+        current = s[i]
+
+# Check the last current substring after the loop ends
+if len(current) > len(longest):
+    longest = current
+
+print("Longest substring in alphabetical order is:", longest)
