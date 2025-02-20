8fd2fd7181cf0169a513a5beed814758707b13b8
Husayn01 <hussainiahmed222@gmail.com>
2025-02-20 11:20:23 +0100

    Practiced MITx Course

--- MITx Course/6.00.1x/P_set/pset_3.3.py
+++ MITx Course/6.00.1x/P_set/pset_3.3.py
@@ -27,48 +27,89 @@
 # you should print a message telling them they've already guessed that - so try again!).
 
 import string
-def getAvailableLetters(lettersGuessed):
-    '''
-    lettersGuessed: list, what letters have been guessed so far
-    returns: string, comprised of letters that represents what letters have not
-      yet been guessed.
-    '''
-    alphabet = string.ascii_lowercase
-    s = ""
-    for letter in alphabet:
-        if letter not in lettersGuessed:
-            s += letter
-    return s
-lettersGuessed = ['e', 'i', 'k', 'p', 'r', 's']
-print(getAvailableLetters(lettersGuessed))
 
-def getGuessedWord(secretWord, lettersGuessed):
+def hangman(secretWord):
     '''
-    secretWord: string, the word the user is guessing
-    lettersGuessed: list, what letters have been guessed so far
-    returns: string, comprised of letters and underscores that represents
-      what letters in secretWord have been guessed so far.
-    '''
-    word = ["_"] * len(secretWord) 
-    for index, letter in enumerate(secretWord):
-        if letter in lettersGuessed:
-            word[index] = letter 
-    return " ".join(word)  
-secretWord = 'apple' 
-lettersGuessed = ['e', 'i', 'k', 'p', 'r', 's']
-print(getGuessedWord(secretWord, lettersGuessed))
+    secretWord: string, the secret word to guess.
 
+    Starts up an interactive game of Hangman.
+
+    * At the start of the game, let the user know how many 
+      letters the secretWord contains.
+
+    * Ask the user to supply one guess (i.e. letter) per round.
+
+    * The user should receive feedback immediately after each guess 
+      about whether their guess appears in the computers word.
+
+    * After each round, you should also display to the user the 
+      partially guessed word so far, as well as letters that the 
+      user has not yet guessed.
+
+    Follows the other limitations detailed in the problem write-up.
+    '''
+    lettersGuessed = []
+    mistakesMade = 0
+    print("Welcome to the game Hangman!")
+    print("I am thinking of a word that is", len(secretWord), "letters long.")
+    print("-------------")
+
+    while True:
+        if isWordGuessed(secretWord, lettersGuessed):
+            print("Congratulations, you won!")
+            break
+        if mistakesMade >= 8:
+            print("Sorry, you ran out of guesses. The word was", secretWord + ".")
+            break
+
+        print("You have", 8 - mistakesMade, "guesses left.")
+        availableLetters = getAvailableLetters(lettersGuessed)
+        print("Available letters:", availableLetters)
+        guess = input("Please guess a letter: ").lower()
+
+        # Validate input
+        if len(guess) != 1:
+            print("Oops! Please enter a single letter.")
+            print("-------------")
+            continue
+        if guess not in string.ascii_lowercase:
+            print("Oops! That is not a valid letter.")
+            print("-------------")
+            continue
+
+        # Check if the letter has already been guessed
+        if guess in lettersGuessed:
+            currentGuessedWord = getGuessedWord(secretWord, lettersGuessed)
+            print("Oops! You've already guessed that letter:", currentGuessedWord)
+            print("-------------")
+            continue
+
+        # Process the new guess
+        lettersGuessed.append(guess)
+        currentGuessedWord = getGuessedWord(secretWord, lettersGuessed)
+
+        if guess in secretWord:
+            print("Good guess:", currentGuessedWord)
+        else:
+            print("Oops! That letter is not in my word:", currentGuessedWord)
+            mistakesMade += 1
+
+        print("-------------")
+
+# Helper functions (as provided in the problem statement)
 def isWordGuessed(secretWord, lettersGuessed):
-    '''
-    secretWord: string, the word the user is guessing
-    lettersGuessed: list, what letters have been guessed so far
-    returns: boolean, True if all the letters of secretWord are in lettersGuessed;
-      False otherwise
-    '''
     for letter in secretWord:
         if letter not in lettersGuessed:
             return False
     return True
-secretWord = 'apple'
-lettersGuessed = ['e', 'i', 'k', 'p', 'r', 's']
-print(isWordGuessed(secretWord, lettersGuessed))
+
+def getGuessedWord(secretWord, lettersGuessed):
+    word = ["_"] * len(secretWord)
+    for index, letter in enumerate(secretWord):
+        if letter in lettersGuessed:
+            word[index] = letter
+    return " ".join(word)
+
+def getAvailableLetters(lettersGuessed):
+    alphabet = string.ascii_lowercase
+    return ''.join([letter for letter in alphabet if letter not in lettersGuessed])
