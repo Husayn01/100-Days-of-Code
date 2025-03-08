c276e30ed40d8e04c121cce55de29ed09c68c40d
Husayn01 <hussainiahmed222@gmail.com>
2025-03-08 20:15:05 +0100

    Completed Day 13 and Day 14 Tasks

--- Day 14/higher_lower_game.py
+++ Day 14/higher_lower_game.py
@@ -5,16 +5,16 @@ from art import *
 print(logo)
 score = 0
 is_game_over = False
+rand_a = random.randint(0, len(data) - 1)
 
 while not is_game_over:
-    rand_a = random.randint(0, len(data) - 1)
     rand_b = random.randint(0, len(data) - 1)
 
     # Ensure rand_b is different from rand_a
     while rand_b == rand_a:
         rand_b = random.randint(0, len(data) - 1)
 
-    # Correct string formatting
+    # string formatting
     print(f"Compare A: {data[rand_a]['name']}, a {data[rand_a]['description']}, from {data[rand_a]['country']}")
     print(vs)
     print(f"Against B: {data[rand_b]['name']}, a {data[rand_b]['description']}, from {data[rand_b]['country']}")
@@ -29,11 +29,12 @@ while not is_game_over:
 
     if guess == highest_follower:
         score += 1
+        print(f"{data[rand_a]['name']}: {data[rand_a]['follower_count']} followers")
+        print(f"{data[rand_b]['name']}: {data[rand_b]['follower_count']} followers")
         print(f"You are right! Current score: {score}")
+        rand_a = rand_b
     else:
         is_game_over = True
         print(f"You are wrong! Final score: {score}")
 
     # Display actual follower counts
-    print(f"{data[rand_a]['name']}: {data[rand_a]['follower_count']} followers")
-    print(f"{data[rand_b]['name']}: {data[rand_b]['follower_count']} followers")
